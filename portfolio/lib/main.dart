0eb777f1550e2574ab4b4f27becd22853b77ccb4
Raj-Surase <rajsurase3@gmail.com>
2024-05-28 12:11:55 +0530

    Initial portfolio  commit

--- portfolio/lib/main.dart
+++ portfolio/lib/main.dart
@@ -1,125 +1,423 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:resume/styles/apptheme.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Portfolio',
+      home: PortfolioScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class PortfolioScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: AppTheme.backgroundColor,
+      // appBar: AppBar(
+      //   backgroundColor: AppTheme.backgroundColor,
+      //   surfaceTintColor: Color.fromARGB(255, 197, 197, 197),
+      //   actions: [
+      //     IconButton(
+      //       icon: Image.asset('assets/twitter.png'),
+      //       onPressed: () => launch('https://x.com/raj-surase'),
+      //     ),
+      //     IconButton(
+      //       icon: Image.asset('assets/linkedin.png'),
+      //       onPressed: () => launch('https://linkedin.com/raj-surase'),
+      //     ),
+      //     IconButton(
+      //       icon: Image.asset('assets/github.png'),
+      //       onPressed: () => launch('https://github.com/raj-surase'),
+      //     ),
+      //     IconButton(
+      //       icon: Image.asset('assets/gmail.png'),
+      //       onPressed: () => launch('mailto:rajsurase3@gmail.com'),
+      //     ),
+      //     IconButton(
+      //       icon: Icon(Icons.picture_as_pdf),
+      //       onPressed: () => launch('https://your-resume-link.com'),
+      //     ),
+      //   ],
+      // ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: AppTheme.screenHeight(context) * 0.2,
+              ),
+              Section1(),
+              SizedBox(height: 32),
+              SizedBox(
+                height: AppTheme.screenHeight(context) * 0.7,
+                child: Section2(),
+              ),
+              SizedBox(height: 32),
+              SizedBox(
+                height: AppTheme.screenHeight(context) * 0.7,
+                child: Section3(),
+              ),
+              SizedBox(height: 32),
+              SizedBox(
+                height: AppTheme.screenHeight(context) * 0.7,
+                child: Section4(),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+class Section1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        bool isLargeScreen = constraints.maxWidth > 600;
+        return isLargeScreen
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: AppTheme.screenWidth(context) * 0.4,
+                    child: _buildTextContent(context),
+                  ),
+                  // SizedBox(width: 16),
+                  Container(
+                    height: AppTheme.screenHeight(context) * 0.4,
+                    width: AppTheme.screenWidth(context) * 0.4,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                          image: AssetImage('assets/profile.jpg')),
+                    ),
+                  ),
+                ],
+              )
+            : Column(
+                children: [
+                  _buildTextContent(context),
+                  SizedBox(height: 16),
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundImage: NetworkImage('https://dummyimage.com/160'),
+                  ),
+                ],
+              );
+      },
+    );
+  }
+
+  Widget _buildTextContent(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('HEY THERE!', style: AppTheme.headline3),
+        SizedBox(height: 8),
+        Text('I\'m Raj Surase', style: AppTheme.headline1),
+        SizedBox(height: 16),
+        Text(
+          'A Flutter Developer with a passion for innovation and a drive to explore new technologies. Always eager to push the boundaries of creativity and functionality!',
+          style: AppTheme.bodyText1,
+        ),
+        SizedBox(height: 16),
+        Row(
+          children: [
+            IconButton(
+              icon: Image.asset(
+                'assets/twitter.png',
+                height: 36,
+                width: 36,
+              ),
+              onPressed: () => launch('https://x.com/raj-surase'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            IconButton(
+              icon: Image.asset(
+                'assets/linkedin.png',
+                height: 36,
+                width: 36,
+              ),
+              onPressed: () => launch('https://linkedin.com/raj-surase'),
+            ),
+            IconButton(
+              icon: Image.asset(
+                'assets/github.png',
+                height: 36,
+                width: 36,
+              ),
+              onPressed: () => launch('https://github.com/raj-surase'),
+            ),
+            IconButton(
+              icon: Image.asset(
+                'assets/gmail.png',
+                height: 36,
+                width: 36,
+              ),
+              onPressed: () => launch('mailto:rajsurase3@gmail.com'),
+            ),
+            IconButton(
+              icon: Icon(Icons.picture_as_pdf),
+              onPressed: () => launch('https://your-resume-link.com'),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        ElevatedButton(
+          onPressed: () => _showSocialMediaDialog(context),
+          child: Text('Connect with me'),
+          style: ButtonStyle(
+              backgroundColor:
+                  WidgetStatePropertyAll(AppTheme.backgroundColor)),
+        ),
+      ],
     );
+  }
+
+  void _showSocialMediaDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('X'),
+                onTap: () => launch('https://x.com'),
+              ),
+              ListTile(
+                leading: Icon(Icons.linked_camera),
+                title: Text('LinkedIn'),
+                onTap: () => launch('https://linkedin.com'),
+              ),
+              ListTile(
+                leading: Icon(Icons.code),
+                title: Text('GitHub'),
+                onTap: () => launch('https://github.com'),
+              ),
+              ListTile(
+                leading: Icon(Icons.email),
+                title: Text('Email'),
+                onTap: () => launch('mailto:yourmail@example.com'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class Section2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('My Career', style: AppTheme.headline1),
+        SizedBox(height: 16),
+        Text(
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et dolor eget massa suscipit suscipit.',
+          style: AppTheme.bodyText1,
+        ),
+        SizedBox(height: 16),
+        LayoutBuilder(
+          builder: (context, constraints) {
+            bool isLargeScreen = constraints.maxWidth > 600;
+            return isLargeScreen
+                ? Row(
+                    children: _buildCareerItems(context),
+                  )
+                : Column(
+                    children: _buildCareerItems(context),
+                  );
+          },
+        ),
+      ],
+    );
+  }
+
+  List<Widget> _buildCareerItems(BuildContext context) {
+    final List<String> items = [
+      'Android',
+      'Web',
+      'Front-End',
+      'Back-End',
+      'Prototyping',
+      'Coding',
+      'Database'
+    ];
+    return items
+        .map((item) => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Chip(
+                label: Text(item, style: AppTheme.bodyText1),
+              ),
+            ))
+        .toList();
+  }
+}
+
+class Section3 extends StatelessWidget {
+  final List<String> projectImages = [
+    'https://dummyimage.com/300.png/09f/fff',
+    'https://dummyimage.com/300.png/09f/fff',
+    'https://dummyimage.com/300.png/09f/fff',
+    'https://dummyimage.com/300.png/09f/fff',
+    'https://dummyimage.com/300.png/09f/fff',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Projects', style: AppTheme.headline1),
+        SizedBox(height: 16),
+        LayoutBuilder(
+          builder: (context, constraints) {
+            int crossAxisCount = constraints.maxWidth > 600 ? 3 : 1;
+            double gridHeight = constraints.maxWidth > 600 ? 600 : 1000;
+            return SizedBox(
+              height: gridHeight,
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: crossAxisCount,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 1,
+                ),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: projectImages.length,
+                itemBuilder: (context, index) {
+                  return _projectItem(projectImages[index]);
+                },
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _projectItem(String imageUrl) {
+    return Stack(
+      children: [
+        Image.network(imageUrl, fit: BoxFit.cover),
+        Positioned.fill(
+          child: Container(
+            color: Colors.black.withOpacity(0.6),
+            child: Center(
+              child: IconButton(
+                icon: Icon(Icons.code, color: Color(0xFF9AA9E6)),
+                onPressed: () => launch('https://github.com/yourrepo'),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Section4 extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _subjectController = TextEditingController();
+  final TextEditingController _bodyController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Contact', style: AppTheme.headline1),
+        SizedBox(height: 16),
+        LayoutBuilder(
+          builder: (context, constraints) {
+            bool isLargeScreen = constraints.maxWidth > 600;
+            return Column(
+              children: [
+                isLargeScreen
+                    ? Row(
+                        children: [
+                          Expanded(
+                              child: _buildTextField(
+                                  _emailController, 'Email', context)),
+                          SizedBox(width: 16),
+                          Expanded(
+                              child: _buildTextField(
+                                  _subjectController, 'Subject', context)),
+                        ],
+                      )
+                    : Column(
+                        children: [
+                          _buildTextField(_emailController, 'Email', context),
+                          SizedBox(height: 16),
+                          _buildTextField(
+                              _subjectController, 'Subject', context),
+                        ],
+                      ),
+                SizedBox(height: 16),
+                _buildTextField(_bodyController, 'Body', context, maxLines: 10),
+                SizedBox(height: 16),
+                ElevatedButton(
+                    onPressed: () => _sendEmail(),
+                    child: Text('Submit'),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            WidgetStatePropertyAll(AppTheme.backgroundColor))),
+              ],
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTextField(
+      TextEditingController controller, String label, BuildContext context,
+      {int maxLines = 1}) {
+    return TextField(
+      controller: controller,
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Color(0xFF8F8F8F)),
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+      style: TextStyle(color: Colors.black),
+    );
+  }
+
+  void _sendEmail() {
+    final Uri emailLaunchUri = Uri(
+      scheme: 'mailto',
+      path: _emailController.text,
+      query: 'subject=${_subjectController.text}&body=${_bodyController.text}',
+    );
+    launch(emailLaunchUri.toString());
   }
 }
---
+++ portfolio/lib/styles/apptheme.dart
@@ -0,0 +1,41 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFF252424);
+  static const Color accentColor = Color(0xFF9AA9E6);
+  static const Color backgroundColor = Color(0xFF252424);
+
+  static TextStyle get headline1 => GoogleFonts.montserrat(
+        fontSize: 36,
+        fontWeight: FontWeight.w900,
+        color: Color(0xFF9AA9E6),
+      );
+
+  static TextStyle get headline2 => GoogleFonts.montserrat(
+        fontSize: 28,
+        fontWeight: FontWeight.w700,
+        color: Color(0xFFDDDEE4),
+      );
+
+  static TextStyle get headline3 => GoogleFonts.montserrat(
+        fontSize: 24,
+        fontWeight: FontWeight.w400,
+        color: Color(0xFFDDDEE4),
+      );
+
+  static TextStyle get bodyText1 => GoogleFonts.montserrat(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        color: Color(0xFFDDDEE4),
+      );
+
+  static double screenWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  static double screenHeight(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
+}
